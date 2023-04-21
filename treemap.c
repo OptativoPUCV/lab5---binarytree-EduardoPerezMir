be59fc04076740e1fbeb2288fea14406a15517e4
Replit user <>
2023-04-21 18:28:18 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -174,7 +174,12 @@ Pair * upperBound(TreeMap * tree, void* key) {
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
-    return NULL;
+    tree->current = tree->root;
+    while (tree->current->left != NULL)
+    {
+        tree->current = tree->current->left;
+    }
+    return tree->current->pair;
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
